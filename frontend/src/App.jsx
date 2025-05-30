b5d640d86d4a53c9d886edfd1d247a283dba70b9
Pranaw Kumar <pramadhu.kumar@gmail.com>
2025-05-31 00:28:02 +0530

    Connected frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+
+        (async () => {
+            try {
+                const response = await fetch('/api/notes')
+                const jsonResponse = await response.json()
+                setNotes(jsonResponse)
+            } catch (error) {
+                console.error('Error fetching notes:', error)
+            }
+        })();
+
+    }, [])
+
+    return (
+        <>
+                {
+                    notes.map ((note) => {
+                        return (
+                            <div key = {note._id}>
+                                <h2>{note.title}</h2>
+                                <p>{note.content}</p>
+                                <p>Category: {note.category}</p>
+                                <p>Created At: {new Date(note.createdAt).toLocaleDateString()}</p>
+                                <p>Updated At: {new Date(note.updatedAt).toLocaleDateString()}</p>
+                            </div>
+                        )
+                    })
+                }
+        </>
+    )
 }
 
 export default App
